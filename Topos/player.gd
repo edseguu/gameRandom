7c1b3bde533bbe6522e79b021a1634c6f16e439d
Eduardo_S <segura2011@live.com>
2025-09-09 14:50:35 -0600

    Feat: [Topos] Se añaden sprites en player

--- Topos/player.gd
+++ Topos/player.gd
@@ -1,4 +1,7 @@
 extends CharacterBody2D
+@onready var sprite_player = $"Sprite2D"
+@onready var sprite_player_smash = preload("res://assets/Topos/player_smash.png")
+@onready var sprite_player_normal = preload("res://assets/Topos/player.png")
 @export var speed = 400
 
 func get_input():
@@ -8,3 +11,11 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+
+
+func _on_area_2d_player_area_entered(area: Area2D) -> void:
+	sprite_player.texture = sprite_player_smash
+
+
+func _on_area_2d_player_area_exited(area: Area2D) -> void:
+	sprite_player.texture = sprite_player_normal
