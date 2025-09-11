f673b4f0bad0b68eb39ce6fdc98b34c09ce40162
Eduardo_S <segura2011@live.com>
2025-09-10 20:39:49 -0600

    Feat: [Game over/Topos] Sprite de wall en topos y cambio random de escenas

--- GameOver/button.gd
+++ GameOver/button.gd
@@ -2,11 +2,13 @@ extends TouchScreenButton
 
 var escenas_random = [
 	"res://ping_pong.tscn",
+	"res://topos.tscn"
 ]
 
 func _on_pressed() -> void:
 	print("POPO")
-	for random in escenas_random:
-		print(random)
-		get_tree().change_scene_to_file(random)
+	var random_scene = escenas_random.pick_random()
+	print(random_scene)
+	get_tree().change_scene_to_file(random_scene)
+
 	
--- Topos/player.gd
+++ Topos/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 @onready var sprite_player = $"Sprite2D"
+@onready var area2D = $Area2DPlayer
 @onready var sprite_player_smash = preload("res://assets/Topos/player_smash.png")
 @onready var sprite_player_normal = preload("res://assets/Topos/player.png")
 @export var speed = 400
@@ -15,6 +16,8 @@ func _physics_process(delta):
 
 func _on_area_2d_player_area_entered(area: Area2D) -> void:
 	sprite_player.texture = sprite_player_smash
+	
+	
 
 
 func _on_area_2d_player_area_exited(area: Area2D) -> void:
